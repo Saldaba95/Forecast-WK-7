3f539793dd56384c60da8296e134c46115b6b03e
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-02-02 10:25:36 -0600

    script/html
    added a function and added some id's

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city-input");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form-engine");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
