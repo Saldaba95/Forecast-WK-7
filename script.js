532cf7d468fadb424de13180afe780b67dac9214
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-02-03 08:11:08 -0600

    Update script.js
    added a function to get the weather to be current, still not operating correctly but still looking it over

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function searchCity(city) {
+  let apiKey = "d44bo97ccfe0cbf0e78f5498fb0fa93t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -7,6 +13,3 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form-engine");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-apiKey = "d44bo97ccfe0cbf0e78f5498fb0fa93t";
-apiUrl = "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
