a0e96e4215f25d8f584656aa74ecd21e69548a40
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-02-07 18:05:25 -0600

    index and script update
    added more id's and added to the function to display current date/time

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ function refreashForecast(response) {
   let weatherElement = document.querySelector("#weather-condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = formatDate(date);
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
-
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
-
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes} `;
+}
 function searchCity(city) {
   let apiKey = "d44bo97ccfe0cbf0e78f5498fb0fa93t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
